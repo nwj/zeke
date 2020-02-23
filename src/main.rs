eaafa0262ec8bdbd9413dcc73cb477994a75033f
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2020-02-23 14:06:13 -0500

    Use clap for command line argument parsing
    Reasoning here is:
    - don't get much by implementing cli parsing myself, and there are a
      surprising number of corner cases to account for if zeke grows into
      anything non-trivial
    - clap looks like the most actively maintained of these rust libraries,
      though their 3.0 version has been cooking for a while. It's also a
      vote of confidence that tools like ripgrep use clap.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("zeke")
+        .version(crate_version!())
+        .about("A tool for managing notes according to the 'Zettelkasten' system")
+        .subcommand(SubCommand::with_name("new").about("Create a new note"))
+        .get_matches();
+
+    if let Some(_) = matches.subcommand_matches("new") {
+        println!("To be implemented");
+    }
 }
