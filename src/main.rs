e5f0e73c240ad63f87395167728326d535e81689
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2020-02-23 16:00:54 -0500

    Dry up crate name reference

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate clap;
 use clap::{App, SubCommand};
 
 fn main() {
-    let matches = App::new("zeke")
+    let matches = App::new(crate_name!())
         .version(crate_version!())
         .about("A tool for managing notes according to the 'Zettelkasten' system")
         .subcommand(SubCommand::with_name("new").about("Create a new note"))
