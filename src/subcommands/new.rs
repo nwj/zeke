455fd3e7594376b4c801195a209b746492b596da
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2020-03-07 13:14:59 -0500

    Add an optional path argument to the new subcommand

--- src/subcommands/new.rs
+++ src/subcommands/new.rs
@@ -7,8 +7,15 @@ use std::io::prelude::*;
 pub fn run(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
     println!("[WIP] Not fully implemented yet. Matches: {:?}", matches);
 
-    let path = format!("{}.md", Local::now().format("%Y%m%d%H%M%S").to_string());
-    let mut file = OpenOptions::new().write(true).create_new(true).open(&path)?;
+    let path = match matches.value_of("PATH") {
+        Some(s) => s.to_string(),
+        None => format!("{}.md", Local::now().format("%Y%m%d%H%M%S").to_string()),
+    };
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(&path)?;
     file.write_all(b"Hello world")?;
 
     println!("Created `{}` note file", &path);
--- tests/subcommands/test_new.rs
+++ tests/subcommands/test_new.rs
@@ -6,11 +6,17 @@ use std::error::Error;
 use std::process::Command;
 use std::str;
 
-#[test]
-fn work_in_progress() -> Result<(), Box<dyn Error>> {
+fn setup() -> Result<(Command, assert_fs::TempDir), Box<dyn Error>> {
     let tempdir = assert_fs::TempDir::new()?;
     let mut cmd = Command::cargo_bin("zeke")?;
     cmd.arg("new").current_dir(tempdir.path());
+
+    Ok((cmd, tempdir))
+}
+
+#[test]
+fn work_in_progress() -> Result<(), Box<dyn Error>> {
+    let (mut cmd, _tempdir) = setup()?;
 
     cmd.assert()
         .success()
@@ -20,19 +26,43 @@ fn work_in_progress() -> Result<(), Box<dyn Error>> {
 }
 
 #[test]
-fn creates_a_note() -> Result<(), Box<dyn Error>> {
-    let tempdir = assert_fs::TempDir::new()?;
-    let mut cmd = Command::cargo_bin("zeke")?;
-    cmd.arg("new").current_dir(tempdir.path());
+fn creates_note() -> Result<(), Box<dyn Error>> {
+    let (mut cmd, tempdir) = setup()?;
 
     let output = cmd.output()?;
     let stdout = str::from_utf8(&output.stdout)?;
     let new_file_name = Regex::new(r"\d{14}.md")?.find(stdout).unwrap().as_str();
-
     tempdir
         .child(new_file_name)
         .assert(predicate::path::is_file());
     tempdir.child(new_file_name).assert("Hello world");
+
+    Ok(())
+}
+
+#[test]
+fn creates_note_with_given_id() -> Result<(), Box<dyn Error>> {
+    let (mut cmd, tempdir) = setup()?;
+    let new_path = "foo.md";
+    cmd.arg(new_path);
+
+    cmd.assert().success();
+    tempdir.child(new_path).assert(predicate::path::is_file());
+    tempdir.child(new_path).assert("Hello world");
+
+    Ok(())
+}
+
+#[test]
+fn does_not_overwrite_existing_files() -> Result<(), Box<dyn Error>> {
+    let (mut cmd, tempdir) = setup()?;
+    let existing_file_path = "foo.md";
+    let existing_file = tempdir.child(existing_file_path);
+    existing_file.touch()?;
+    cmd.arg(existing_file_path);
+
+    cmd.assert().failure();
+    existing_file.assert("");
 
     Ok(())
 }
