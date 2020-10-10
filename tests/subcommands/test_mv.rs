87eb1ee7823b9eb21b624147fa6a3f85b0aced72
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2020-10-10 13:19:01 -0400

    Update mv subcommand tests to use path copy

--- tests/subcommands/test_mv.rs
+++ tests/subcommands/test_mv.rs
@@ -3,29 +3,19 @@ use anyhow::Result;
 use assert_cmd::prelude::*;
 use assert_fs::prelude::*;
 use predicates::prelude::*;
+use std::fs;
 
 #[test]
 fn renames_note() -> Result<()> {
     let t = ZekeTester::new();
-    let old_path = "20200819-foo.md";
-    let content = "---
-title: Foo
-created: \"2020-08-19T18:23:24.774140Z\"
-tags: []
-links: []
---- ";
-    let new_title = "Bar";
-    let new_path = "20200819-bar.md";
+    t.setup_fs("subcommands/test_mv/renames_note/")?;
 
-    t.temp_dir.child(old_path).write_str(content)?;
-
-    t.zeke_mv(old_path, new_title)?.assert().success();
-
+    t.zeke_mv("20201010-a.md", "B")?.assert().success();
     t.temp_dir
-        .child(new_path)
+        .child("20201010-b.md")
         .assert(predicate::path::is_file());
     t.temp_dir
-        .child(old_path)
+        .child("20201010-a.md")
         .assert(predicate::path::missing());
 
     Ok(())
@@ -34,52 +24,26 @@ links: []
 #[test]
 fn updates_note_title() -> Result<()> {
     let t = ZekeTester::new();
-    let old_path = "20200819-foo.md";
-    let content = "---
-title: Foo
-created: \"2020-08-19T18:23:24.774140Z\"
-tags: []
-links: []
---- ";
-    let new_title = "Bar";
-    let new_path = "20200819-bar.md";
+    t.setup_fs("subcommands/test_mv/renames_note/")?;
 
-    t.temp_dir.child(old_path).write_str(content)?;
-
-    t.zeke_mv(old_path, new_title)?.assert().success();
-
+    t.zeke_mv("20201010-a.md", "B")?.assert().success();
     t.temp_dir
-        .child(new_path)
-        .assert(predicate::str::contains("---\ntitle: Bar"));
+        .child("20201010-b.md")
+        .assert(predicate::str::contains("---\ntitle: B"));
 
     Ok(())
 }
 
 #[test]
-fn does_not_modify_other_content_of_the_moved_note() -> Result<()> {
+fn does_not_modify_other_aspects_of_moved_note() -> Result<()> {
     let t = ZekeTester::new();
-    let path = "a.md";
-    let content = "---
-title: A
-created: \"2020-08-19T18:23:24.774140Z\"
-tags:
-  - quisquam
-links:
-  - bar.md
-foo: 12345
----
-Perspiciatis dolores corrupti sit.
-Esse cumque saepe laboriosam.";
-    let new_title = "C";
-    let new_path = "20200819-c.md";
+    t.setup_fs("subcommands/test_mv/does_not_modify_other_aspects_of_moved_note/")?;
+    let initial_content = fs::read_to_string(t.temp_dir.child("20201010-a.md").path())?;
 
-    t.temp_dir.child(path).write_str(&content)?;
-
-    t.zeke_mv(path, new_title)?.assert().success();
-
+    t.zeke_mv("20201010-a.md", "B")?.assert().success();
     t.temp_dir
-        .child(new_path)
-        .assert(content.replace("title: A", "title: C"));
+        .child("20201010-b.md")
+        .assert(initial_content.replace("title: A", "title: B"));
 
     Ok(())
 }
@@ -87,28 +51,13 @@ Esse cumque saepe laboriosam.";
 #[test]
 fn updates_front_matter_linked_notes() -> Result<()> {
     let t = ZekeTester::new();
-    let path_a = "a.md";
-    let path_b = "b.md";
-    let content_b = format!(
-        "---
-title: B
-created: \"2020-08-19T18:23:24.774140Z\"
-tags: []
-links:
-  - {}
----
-Perspiciatis dolores corrupti sit.
-Esse cumque saepe laboriosam.",
-        path_a
-    );
-    t.temp_dir.child(path_a).touch()?;
-    t.temp_dir.child(path_b).write_str(&content_b)?;
+    t.setup_fs("subcommands/test_mv/updates_front_matter_linked_notes/")?;
 
-    t.zeke_mv(path_a, "C")?.assert().success();
+    t.zeke_mv("20201010-a.md", "C")?.assert().success();
     t.temp_dir
-        .child(path_b)
-        .assert(predicate::str::contains(format!("links:\n  - {}\n", path_a,)).not())
-        .assert(predicate::str::contains("links:\n  - c.md\n"));
+        .child("20201010-b.md")
+        .assert(predicate::str::contains("links:\n  - 20201010-a.md\n").not())
+        .assert(predicate::str::contains("links:\n  - 20201010-c.md\n"));
 
     Ok(())
 }
@@ -116,27 +65,13 @@ Esse cumque saepe laboriosam.",
 #[test]
 fn updates_content_linked_notes() -> Result<()> {
     let t = ZekeTester::new();
-    let path_a = "a.md";
-    let path_b = "b.md";
-    let content_b = format!(
-        "---
-title: B
-created: \"2020-08-19T18:23:24.774140Z\"
-tags: []
-links: []
----
-Perspiciatis dolores [corrupti]({}) sit.
-Esse cumque saepe laboriosam.",
-        path_a
-    );
-    t.temp_dir.child(path_a).touch()?;
-    t.temp_dir.child(path_b).write_str(&content_b)?;
+    t.setup_fs("subcommands/test_mv/updates_content_linked_notes/")?;
 
-    t.zeke_mv(path_a, "C")?.assert().success();
+    t.zeke_mv("20201010-a.md", "C")?.assert().success();
     t.temp_dir
-        .child(path_b)
-        .assert(predicate::str::contains(format!("[corrupti]({})", path_a,)).not())
-        .assert(predicate::str::contains("[corrupti](c.md)"));
+        .child("20201010-b.md")
+        .assert(predicate::str::contains("[corrupti](20201010-a.md)").not())
+        .assert(predicate::str::contains("[corrupti](20201010-c.md)"));
 
     Ok(())
 }
@@ -144,29 +79,13 @@ Esse cumque saepe laboriosam.",
 #[test]
 fn does_not_modify_other_aspects_of_linked_notes() -> Result<()> {
     let t = ZekeTester::new();
-    let path_a = "a.md";
-    let path_b = "b.md";
-    let content_b = format!(
-        "---
-title: B
-created: \"2020-08-19T18:23:24.774140Z\"
-tags:
-  - a.md
-links:
-  - {}
-other_stuff: whatever
----
-Perspiciatis dolores corrupti sit.
-Esse cumque saepe laboriosam.",
-        path_a
-    );
-    t.temp_dir.child(path_a).touch()?;
-    t.temp_dir.child(path_b).write_str(&content_b)?;
+    t.setup_fs("subcommands/test_mv/does_not_modify_other_aspects_of_linked_notes/")?;
+    let initial_content = fs::read_to_string(t.temp_dir.child("20201010-b.md").path())?;
 
-    t.zeke_mv(path_a, "C")?.assert().success();
+    t.zeke_mv("20201010-a.md", "C")?.assert().success();
     t.temp_dir
-        .child(path_b)
-        .assert(content_b.replace(&format!("links:\n  - {}", path_a), "links:\n  - c.md"));
+        .child("20201010-b.md")
+        .assert(initial_content.replace("links:\n  - 20201010-a.md", "links:\n  - 20201010-c.md"));
 
     Ok(())
 }
