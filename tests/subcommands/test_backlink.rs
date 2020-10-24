51a7df610a6b1a008f21d63960d30f59649b4127
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2020-10-24 14:18:31 -0400

    Use path copy for backlink tests

--- tests/subcommands/test_backlink.rs
+++ tests/subcommands/test_backlink.rs
@@ -5,49 +5,21 @@ use assert_fs::prelude::*;
 use predicates::prelude::*;
 
 #[test]
-fn adds_links_to_notes_references_by_other_notes() -> Result<()> {
+fn links_notes_referenced_by_other_notes() -> Result<()> {
     let t = ZekeTester::new();
-    let path1 = "a.md";
-    let path2 = "b.md";
-    let path3 = "c.md";
-    let content1 = "---
-title: A
-created: \"2020-04-19T18:23:24.774140Z\"
-tags: []
-links: []
----
-This is a reference to [B](b.md) and [C](c.md)
-";
-    let content2 = "---
-title: B
-created: \"2020-04-19T18:23:24.774140Z\"
-tags: []
-links: []
----
-No links to be found here.";
-    let content3 = "---
-title: C
-created: \"2020-04-19T18:23:24.774140Z\"
-tags: []
-links: []
----
-This is a reference to [somewhere else](b.md)";
-
-    t.temp_dir.child(path1).write_str(content1)?;
-    t.temp_dir.child(path2).write_str(content2)?;
-    t.temp_dir.child(path3).write_str(content3)?;
+    t.setup_fs("subcommands/test_backlink/links_notes_referenced_by_other_notes")?;
 
     t.zeke_backlink()?.assert().success();
 
     t.temp_dir
-        .child(path2)
-        .assert(predicate::str::contains(path1));
+        .child("20201024-b.md")
+        .assert(predicate::str::contains("20201024-a.md"));
     t.temp_dir
-        .child(path2)
-        .assert(predicate::str::contains(path3));
+        .child("20201024-c.md")
+        .assert(predicate::str::contains("20201024-a.md"));
     t.temp_dir
-        .child(path3)
-        .assert(predicate::str::contains(path1));
+        .child("20201024-c.md")
+        .assert(predicate::str::contains("20201024-b.md"));
 
     Ok(())
 }
@@ -61,27 +33,15 @@ fn does_not_panic_on_directories() -> Result<()> {
 }
 
 #[test]
-fn idempotent_if_invoked_repeatedly() -> Result<()> {
+fn idempotent() -> Result<()> {
     let t = ZekeTester::new();
-    let path1 = "a.md";
-    let path2 = "b.md";
-    let content1 = "---
-title: A
-created: \"2020-04-19T18:23:24.774140Z\"
-tags: []
-links: []
----
-This is a reference to [B](b.md)
-";
-    t.temp_dir.child(path1).write_str(content1)?;
-    t.temp_dir.child(path2).touch()?;
+    t.setup_fs("subcommands/test_backlink/idempotent")?;
 
     t.zeke_backlink()?.assert().success();
     t.zeke_backlink()?.assert().success();
-
     t.temp_dir
-        .child(path2)
-        .assert(predicate::str::contains(path1).count(1));
+        .child("20201024-b.md")
+        .assert(predicate::str::contains("20201024-a.md").count(1));
 
     Ok(())
 }
