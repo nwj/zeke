8249896bd814fe8f53632e8b4cd1368043d30ba8
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2020-12-23 22:18:40 -0500

    Parallelize backlink subcommand's file reads

--- src/subcommands/backlink.rs
+++ src/subcommands/backlink.rs
@@ -1,27 +1,26 @@
 use crate::fs::{read_dir, read_note, write_note};
 use anyhow::Result;
 use path_clean::PathClean;
+use rayon::prelude::*;
 
 pub fn run() -> Result<()> {
-    for entry in read_dir("./")?.filter_map(|r| r.ok()) {
-        let p = &entry.path();
+    let entries: Vec<_> = read_dir("./")?.filter_map(|r| r.ok()).collect();
 
-        let note = match read_note(&p) {
-            Ok(n) => n,
-            Err(_) => continue,
-        };
+    let link_map: Vec<(_, _)> = entries
+        .par_iter()
+        .map(|e| read_note(e.path()))
+        .filter_map(|r| r.ok())
+        .map(|n| (n.path.clone().unwrap(), n.content.get_note_links()))
+        .collect();
 
-        for link in note.content.get_note_links().iter() {
+    for (path, links) in link_map.iter() {
+        for link in links.iter() {
             let mut linked_note = match read_note(link) {
                 Ok(n) => n,
                 Err(_) => continue,
             };
 
-            if linked_note
-                .front_matter
-                .links
-                .insert(note.path.clone().unwrap().clean())
-            {
+            if linked_note.front_matter.links.insert(path.clean()) {
                 write_note(&linked_note, false)?;
             }
         }
