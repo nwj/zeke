b1d34ece8da35c59971e90c32c41ff4949f539cb
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2020-12-27 15:10:14 -0500

    Parallelize backlink subcommand's file writing
    This does some additional work to invert the link map, in order to
    ensure that we only write to each possible file once. With that
    assurance in place though, the file writing can be done in parallel.

--- src/subcommands/backlink.rs
+++ src/subcommands/backlink.rs
@@ -3,9 +3,11 @@ use anyhow::Result;
 use path_clean::PathClean;
 use rayon::iter::Either;
 use rayon::prelude::*;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn run() -> Result<i32> {
-    let (link_map, errs): (Vec<_>, Vec<_>) =
+    let (link_map, mut errs): (Vec<_>, Vec<_>) =
         read_dir("./")
             .par_bridge()
             .partition_map(|en| match read_note(en.path()) {
@@ -18,26 +20,42 @@ pub fn run() -> Result<i32> {
         eprintln!("{:?}", e);
     }
 
-    for (path, links) in link_map {
-        for link in links {
-            let mut linked_note = match read_note(link) {
-                Ok(n) => n,
-                Err(e) => {
-                    eprintln!("{:?}", e);
-                    err_count += 1;
-                    continue;
-                }
-            };
-
-            if linked_note.front_matter.links.insert(path.clean()) {
-                if let Err(e) = write_note(&linked_note, false) {
-                    eprintln!("{:?}", e);
-                    err_count += 1;
-                };
+    let inverse_link_map = link_map
+        .iter()
+        .fold(HashMap::new(), |mut acc, (path, links)| {
+            for l in links {
+                let inverse_links = acc.entry(l).or_insert(HashSet::new());
+                inverse_links.insert(path);
             }
+            acc
+        });
+
+     errs = inverse_link_map.par_iter().filter_map(|(path, links)| {
+        let mut linked_note = match read_note(path) {
+            Ok(n) => n,
+            Err(e) => {
+                return Some(e)
+            }
+        };
+
+        let insert_results: Vec<_> = links
+            .iter()
+            .map(|l| linked_note.front_matter.links.insert(l.clean()))
+            .collect();
+        if insert_results.iter().any(|&b| b) {
+            if let Err(e) = write_note(&linked_note, false) {
+                return Some(e)
+            };
         }
+
+        None
+    }).collect();
+
+    err_count += errs.len();
+    for e in errs {
+        eprintln!("{:?}", e);
     }
 
     eprintln!("Backlinking done.");
-    Ok(if err_count > 0 {1} else {0})
+    Ok(if err_count > 0 { 1 } else { 0 })
 }
