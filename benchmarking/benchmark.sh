ba85e6c4914e3bff6c8b5851e83c6270da37ff37
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2021-10-02 20:15:20 -0400

    Improve benchmarking tests
    Improvements here include:
    - complete teardown/rebuild of file system state between test runs
    - better shell scripting practices
    - testing of all of zeke's subcommands
    - a pattern for keeping reports of past benchmarks

--- benchmarking/benchmark.sh
+++ benchmarking/benchmark.sh
@@ -1,39 +1,87 @@
 #!/bin/bash
 
-# Configuration
-WARMUP_COUNT=5
-MIN_RUNS=50
-DIR_SIZE=1000
+function create_note () {
+  local file_name="$1"
+  local tag="$2"
+  local link="$3"
 
-# Commands to benchmark
-COMMAND_BACKLINK="zeke backlink"
-COMMAND_MV="zeke mv"
-COMMAND_TAGS="zeke tags"
+  local file_content
+  file_content="---\ntitle: ${file_name}\ncreated: \"2021-09-07T16:44:27.201207Z\"\n"
 
-if ! which hyperfine > /dev/null 2>&1; then
-	echo "'hyperfine' does not seem to be installed."
-	echo "You can get it here: https://github.com/sharkdp/hyperfine"
-	exit 1
-fi
+  if [[ -n "${tag}" ]]; then
+    file_content+="tags:\n  - ${tag}\n"
+  else
+    file_content+="tags: []\n"
+  fi
 
-if ! which zeke > /dev/null 2>&1; then
-	echo "'zeke' does not seem to be installed."
-	echo "You can get it here: https://github.com/nwj/zeke"
-	exit 1
-fi
+  if [[ -n "${link}" ]]; then
+    file_content+="links:\n  - ${link}\n"
+  else
+    file_content+="links: []\n"
+  fi
 
-for ((i=0;i<$DIR_SIZE;i++));
-do
-	echo -ne "Creating $i / $DIR_SIZE temp files...\r"
-	zeke new "$RANDOM$RANDOM" > /dev/null 2>&1
-done
-echo -e "$(tput el)Created $DIR_SIZE temp files.\n"
+  file_content+="---\nLorem ipsum dolor sit amet, consectetur adipiscing elit."
 
-hyperfine --warmup $WARMUP_COUNT --min-runs $MIN_RUNS \
-	"$COMMAND_BACKLINK" \
-	"$COMMAND_MV \$(ls | head -n 1) \$RANDOM\$RANDOM" \
-	"$COMMAND_TAGS"
+  echo -e "${file_content}" > "${file_name}"
+}
 
-echo -ne "\nCleaning up temp files...\r"
-find . -type f -name "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-*.md" -delete
-echo -e "$(tput el)Cleaned up temp files."
+function before_all () {
+  mkdir -p "tmp/template"
+
+  local tags
+  local links
+
+  tags=(alpha beta gamma delta)
+  links=(4.md 3.md 2.md 1.md)
+
+  local mod
+  local filename
+  local tag
+  local link
+  for i in {0..99}; do
+    mod=$((i % 5))
+    filename="tmp/template/${i}.md"
+
+    if [[ "${i}" -lt 5 || "${mod}" == 4 ]]; then
+      create_note "${filename}"
+    else
+      tag="${tags[$mod]}"
+      link="${links[$mod]}"
+      create_note "${filename}" "${tag}" "${link}"
+    fi
+  done
+
+  export -f before_each
+  export -f t
+}
+
+function before_each () {
+  rm -rf tmp/test
+  cp -R tmp/template tmp/test
+}
+
+function after_all () {
+  rm -rf ./tmp
+}
+
+function t () {
+  pushd ./tmp/test && "$@" && pushd || exit
+}
+
+function main () {
+  before_all
+
+  hyperfine --warmup 10 --prepare before_each "t zeke new 'foo'" \
+    "t zeke mv '1.md' 'foo.md'" \
+    "t zeke tag 'gamma' '1.md'" \
+    "t zeke untag 'alpha' '5.md'" \
+    "t zeke tags" \
+    "t zeke link 1.md 2.md" \
+    "t zeke link 1.md 2.md" \
+    "t zeke unlink 4.md 5.md" \
+    "t zeke backlink"
+
+  after_all
+}
+
+main "$@"
