cedc17e6944caa2e630f7b6ebc9a65c3e6a9d8b0
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2024-10-15 12:19:51 -0400

    Setup basic error handling flow with anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use anyhow::Result;
+
+fn main() {
+    match run() {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("Error: {:#}", e);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn run() -> Result<()> {
+    Ok(())
+}
